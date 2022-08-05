0a23da14adb6c52776f7e70956f2bccf87cd1813
Maximo Silva <42160673+meisimo@users.noreply.github.com>
2022-08-05 12:16:07 -0500

    The promises handling now implements the Promise.any that in more robust and flexible.

--- src/index.js
+++ src/index.js
@@ -2,29 +2,35 @@ async function cache_store(key, value) {}
 async function cache_retrive(key) {}
 async function slow_function(input) {}
 
-//This function allows the depency injection from the cache.
+//This function allows the inject cache dependency for better testing.
 function cached_function(cache_store, cache_retrive, callback) {
+    async function callback_and_cache(input) {
+        const result = await callback(input);
+
+        cache_store(input, result);
+
+        return result;
+    }
+
+    async function cache_retrieve_wrap(input) {
+        const result = await cache_retrive(input);
+        
+        if (result === undefined)
+            throw Error(`undefined value return from cache for key: ${input}`);
+        
+        return result;
+    }
+
     return (input) => {
-        return new Promise(resolve => {
-
-            cache_retrive(input)
-                .then(value => {
-                    if (value !== undefined) {
-                        resolve(value);
-                    }
-                });
-
-            callback(input)
-                .then(value => {
-                    cache_store(input, value);
-                    resolve(value)
-                });
-        });
+        return Promise.any([callback_and_cache(input), cache_retrieve_wrap(input)])
     }
 }
 
 function memoize(slow_function) {
-    return cached_function(cache_store, cache_retrive, slow_function);
+    const fast_function = cached_function(
+        cache_store, cache_retrive, slow_function)
+
+    return fast_function;
 }
 
 module.exports = {
